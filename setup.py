a84239b6f6fd90744a0aa84fe4ac179905306841
Otavio Trentin <otavio_trentin@hotmail.com>
2026-04-15 15:41:21 -0400

    remove singer dependency cause it is inside hotglue-singer-sdk

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name='tap-slack',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_slack'],
       install_requires=[
-          'singer-python==5.9.0',
           'hotglue-singer-sdk~=1.0.26',
           'slackclient==2.6.0',
       ],
